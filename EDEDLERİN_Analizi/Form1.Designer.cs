7d24c7c64f6fe70290461b1d0bdd88107d03a4e6
Nazila Valiyeva (Nazilə Vəliyeva) <Nazilaveliyeva94@gmail.com>
2025-05-14 14:37:31 +0400

    Add project files.

---
+++ EDEDLERİN_Analizi/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace EDEDLERİN_Analizi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFaktorial = new System.Windows.Forms.Button();
+            this.btnSadelik = new System.Windows.Forms.Button();
+            this.btnTersi = new System.Windows.Forms.Button();
+            this.btnReqemCemi = new System.Windows.Forms.Button();
+            this.btnBolunme5 = new System.Windows.Forms.Button();
+            this.btnKvadratKub = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(38, 61);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(387, 72);
+            this.txtInput.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(410, 38);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Nəticə burada göstəriləcək.";
+            // 
+            // btnFaktorial
+            // 
+            this.btnFaktorial.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFaktorial.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFaktorial.ForeColor = System.Drawing.Color.Red;
+            this.btnFaktorial.Location = new System.Drawing.Point(304, 200);
+            this.btnFaktorial.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnFaktorial.Name = "btnFaktorial";
+            this.btnFaktorial.Size = new System.Drawing.Size(121, 48);
+            this.btnFaktorial.TabIndex = 13;
+            this.btnFaktorial.Text = "Faktorial";
+            this.btnFaktorial.UseVisualStyleBackColor = true;
+            // 
+            // btnSadelik
+            // 
+            this.btnSadelik.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSadelik.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSadelik.ForeColor = System.Drawing.Color.Red;
+            this.btnSadelik.Location = new System.Drawing.Point(171, 200);
+            this.btnSadelik.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnSadelik.Name = "btnSadelik";
+            this.btnSadelik.Size = new System.Drawing.Size(121, 48);
+            this.btnSadelik.TabIndex = 12;
+            this.btnSadelik.Text = "Sade eded";
+            this.btnSadelik.UseVisualStyleBackColor = true;
+            // 
+            // btnTersi
+            // 
+            this.btnTersi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTersi.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTersi.ForeColor = System.Drawing.Color.Red;
+            this.btnTersi.Location = new System.Drawing.Point(38, 200);
+            this.btnTersi.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnTersi.Name = "btnTersi";
+            this.btnTersi.Size = new System.Drawing.Size(121, 48);
+            this.btnTersi.TabIndex = 11;
+            this.btnTersi.Text = "Ededin tersi";
+            this.btnTersi.UseVisualStyleBackColor = true;
+            // 
+            // btnReqemCemi
+            // 
+            this.btnReqemCemi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReqemCemi.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReqemCemi.ForeColor = System.Drawing.Color.Red;
+            this.btnReqemCemi.Location = new System.Drawing.Point(304, 146);
+            this.btnReqemCemi.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnReqemCemi.Name = "btnReqemCemi";
+            this.btnReqemCemi.Size = new System.Drawing.Size(121, 48);
+            this.btnReqemCemi.TabIndex = 10;
+            this.btnReqemCemi.Text = "Reqem Cemi";
+            this.btnReqemCemi.UseVisualStyleBackColor = true;
+            // 
+            // btnBolunme5
+            // 
+            this.btnBolunme5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBolunme5.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBolunme5.ForeColor = System.Drawing.Color.Red;
+            this.btnBolunme5.Location = new System.Drawing.Point(171, 146);
+            this.btnBolunme5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnBolunme5.Name = "btnBolunme5";
+            this.btnBolunme5.Size = new System.Drawing.Size(121, 48);
+            this.btnBolunme5.TabIndex = 9;
+            this.btnBolunme5.Text = "5 Bolunme";
+            this.btnBolunme5.UseVisualStyleBackColor = true;
+            // 
+            // btnKvadratKub
+            // 
+            this.btnKvadratKub.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKvadratKub.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKvadratKub.ForeColor = System.Drawing.Color.Red;
+            this.btnKvadratKub.Location = new System.Drawing.Point(38, 146);
+            this.btnKvadratKub.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnKvadratKub.Name = "btnKvadratKub";
+            this.btnKvadratKub.Size = new System.Drawing.Size(121, 48);
+            this.btnKvadratKub.TabIndex = 8;
+            this.btnKvadratKub.Text = "KvadratKub";
+            this.btnKvadratKub.UseVisualStyleBackColor = true;
+            this.btnKvadratKub.Click += new System.EventHandler(this.btnKvadratKub_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 276);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFaktorial);
+            this.Controls.Add(this.btnSadelik);
+            this.Controls.Add(this.btnTersi);
+            this.Controls.Add(this.btnReqemCemi);
+            this.Controls.Add(this.btnBolunme5);
+            this.Controls.Add(this.btnKvadratKub);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFaktorial;
+        private System.Windows.Forms.Button btnSadelik;
+        private System.Windows.Forms.Button btnTersi;
+        private System.Windows.Forms.Button btnReqemCemi;
+        private System.Windows.Forms.Button btnBolunme5;
+        private System.Windows.Forms.Button btnKvadratKub;
+    }
+}
+
---
+++ EDEDLERİN_Analizi/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EDEDLERİN_Analizi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// İstifadəçinin daxil etdiyi ədədi tam ədədə çevirmək üçün istifadə olunur.
+        /// Əgər çevrilə bilməzsə, false qaytarır.
+        /// </summary>
+        private bool TryGetInput(out int result)
+        {
+            return int.TryParse(txtInput.Text, out result);
+        }
+        private void EdedAnalizApp_Load(object sender, EventArgs e)
+        {
+            txtInput.Focus();
+            txtInput.ForeColor = System.Drawing.Color.DarkRed;
+            //txtInput.BackColor = System.Drawing.Color.LightYellow;
+            txtInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            //txtInput.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            txtInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            label1.Text = "Nəticə burada göstəriləcək.";
+            label1.ForeColor = System.Drawing.Color.Blue;
+            //label1.BackColor = System.Drawing.Color.LightGray;
+            //label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            //label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+
+        }
+        /// <summary>
+        /// Kvadrat və ya kub olub olmadığını yoxlayır.
+        /// </summary>
+
+        private void btnKvadratKub_Click(object sender, EventArgs e)
+        {
+            if (!TryGetInput(out int number))
+            {
+                MessageBox.Show("Zəhmət olmasa düzgün tam ədəd daxil edin.");
+                return;
+            }
+
+            bool isSquare = false;
+            bool isCube = false;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i * i == number)
+                    isSquare = true;
+                if (i * i * i == number)
+                    isCube = true;
+            }
+
+            if (isSquare && isCube)
+                label1.Text = "Bu ədəd həm kvadrat, həm də kubdur.";
+            else if (isSquare)
+                label1.Text = "Bu ədəd kvadratdır.";
+            else if (isCube)
+                label1.Text = "Bu ədəd kubdur.";
+            else
+                label1.Text = "Bu ədəd nə kvadrat, nə də kubdur.";
+        }
+    }
+}
